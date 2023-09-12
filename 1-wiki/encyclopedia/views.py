12fca522b336060a11c207f302088a125055660e
Nelson Mendez <105968684+nelsonmenza@users.noreply.github.com>
2023-09-12 09:19:49 -0700

    Improved comments and docstring in views and fixed bugs in edit.html

--- 1-wiki/encyclopedia/views.py
+++ 1-wiki/encyclopedia/views.py
@@ -6,31 +6,76 @@ from markdown2 import Markdown
 from . import util
 
 
+# Define the index view that renders a list of encyclopedia entries
 def index(request):
+    """
+    Renders the main encyclopedia index page.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: A response containing the rendered HTML template with a list of encyclopedia entries.
+    """
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()})
+        "entries": util.list_entries()
+    })
+
+# Define the entry view that displays the content of a specific encyclopedia entry
 
 
 def entry(request, title):
+    """
+    Renders the page for a specific encyclopedia entry.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+        title (str): The title of the encyclopedia entry to be displayed.
+
+    Returns:
+        HttpResponse: A response containing the rendered HTML template with the content of the specified entry.
+    """
     markdowner = Markdown()
     page = util.get_entry(title)
-    if page == None:
+    if page is None:
         return render(request, "encyclopedia/error.html",
                       {"title": "Page not found",
-                       "content": "Page not exist or not found."})
+                       "content": "Page does not exist or was not found."})
     else:
         page_converted = markdowner.convert(page)
         return render(request, "encyclopedia/entry.html", {"content": page_converted,
                                                            "title": title})
 
+# Define a view to display a random encyclopedia entry
+
 
 def random_entry(request):
+    """
+    Redirects to a random encyclopedia entry.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: A response that redirects to a randomly selected encyclopedia entry page.
+    """
     entry_list = util.list_entries()
     random_entry = random.choice(entry_list)
     return entry(request, random_entry)
 
+# Define a view to search for encyclopedia entries
+
 
 def search_entry(request):
+    """
+    Searches for encyclopedia entries based on user input.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: A response containing search results or an error message if no results are found.
+    """
     entry_list = util.list_entries()
     if request.method == "POST":
         title = request.POST["q"]
@@ -38,46 +83,78 @@ def search_entry(request):
             return entry(request, title)
         else:
             lst = []
-            for entry in entry_list:
-                if title.lower() in entry.lower():
-                    print(entry)
-                    lst.append(entry)
-            if lst == []:
+            for entri in entry_list:
+                if title.lower() in entri.lower():
+                    lst.append(entri)
+            if not lst:
                 return render(request, "encyclopedia/error.html",
                               {"title": "Search",
-                               "content": "Page not exist or not found."})
+                               "content": "Page does not exist or was not found."})
             else:
                 return render(request, "encyclopedia/search.html", {"entries": lst,
-                                                                    "title": "search suggection"})
+                                                                    "title": "Search Suggestions"})
+
+# Define a view to create a new encyclopedia entry
 
 
 def new_entry(request):
+    """
+    Renders the page for creating a new encyclopedia entry or processes the form submission.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: A response containing the new entry or an error message if the entry already exists.
+    """
     if request.method == "GET":
         return render(request, "encyclopedia/new_entry.html")
     else:
         title = request.POST["title"]
         content = request.POST["content"]
         entry_exist = util.get_entry(title)
-        if entry_exist == None:
+        if entry_exist is None:
             util.save_entry(title, content)
             return entry(request, title)
         else:
             return render(request, "encyclopedia/error.html",
                           {"title": "New page",
-                           "content": "Page already exist",
+                           "content": "Page already exists",
                            "information": "Change the title or edit the current entry."
                            })
 
+# Define a view to edit an existing encyclopedia entry
+
 
 def edit(request):
+    """
+    Renders the page for editing an existing encyclopedia entry.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: A response containing the content of the entry to be edited.
+    """
     if request.method == "POST":
         title = request.POST["entry_title"]
         content = util.get_entry(title)
         return render(request, "encyclopedia/edit.html", {"title": title,
                                                           "content": content})
 
+# Define a view to save changes made to an edited entry
+
 
 def save_edit(request):
+    """
+    Saves the changes made to an edited encyclopedia entry.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: A response that redirects to the edited entry page.
+    """
     if request.method == "POST":
         title = request.POST["title"]
         content = request.POST["content"]
---
+++ 1-wiki/entries/Test.md
@@ -0,0 +1,61 @@
+# Markdown Example
+
+This is a sample Markdown document that demonstrates various Markdown formatting elements.
+
+## Headers
+
+You can create headers with the `#` symbol. There are six levels of headers, with `#` being the largest and `######` the smallest:
+
+# Heading 1
+## Heading 2
+### Heading 3
+#### Heading 4
+##### Heading 5
+###### Heading 6
+
+## Emphasis
+
+You can make text *italic* by enclosing it in single asterisks (`*`) or underscores (`_`). To make text **bold**, use double asterisks (`**`) or double underscores (`__`).
+
+## Lists
+
+### Unordered List
+
+- Item 1
+- Item 2
+  - Subitem 2.1
+  - Subitem 2.2
+- Item 3
+
+### Ordered List
+
+1. First item
+2. Second item
+3. Third item
+
+## Links
+
+You can create links like this: [Visit OpenAI](https://www.openai.com/)
+
+## Images
+
+You can embed images with an exclamation mark followed by square brackets for alt text and parentheses for the image URL:
+
+![OpenAI Logo](https://www.openai.com/images/openai-logo.png)
+
+## Blockquotes
+
+> This is a blockquote.
+> You can add multiple lines to it.
+
+## Code
+
+Inline `code` can be written using backticks.
+
+### Code Blocks
+
+You can create code blocks by enclosing code in triple backticks with an optional language identifier:
+
+```python
+def hello_world():
+    print("Hello, World!")
