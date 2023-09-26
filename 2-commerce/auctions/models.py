b9b56cc99f9ae01e3cf5494e1c50e427793622d3
Nelson Mendez <105968684+nelsonmenza@users.noreply.github.com>
2023-09-26 16:03:26 -0700

    Migrations are made. Improved index.html, and views.py

--- 2-commerce/auctions/models.py
+++ 2-commerce/auctions/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator
 from django.db import models
@@ -7,21 +8,49 @@ class User(AbstractUser):
     pass
 
 
+class Category(models.Model):
+    CLOTHING = 'Apparel and Clothing'
+    TOYS = 'Toys and Games'
+    ELECTRONICS = 'Electronics and Gadgets'
+    HOME = 'Home and Living'
+    SPORTS = 'Sports and Outdoor'
+    BEAUTY = 'Beauty and Personal Care'
+    BOOKS = 'Books and Media'
+    FOOD = 'Food and Grocery'
+    AUTOMOTIVE = 'Automotive and Parts'
+    HEALTH = 'Health and Wellness'
+
+    CATEGORIES_CHOICES = (
+        (CLOTHING, 'Apparel and Clothing'),
+        (TOYS, 'Toys and Games'),
+        (ELECTRONICS, 'Electronics and Gadgets'),
+        (HOME, 'Home and Living'),
+        (SPORTS, 'Sports and Outdoor'),
+        (BEAUTY, 'Beauty and Personal Care'),
+        (BOOKS, 'Books and Media'),
+        (FOOD, 'Food and Grocery'),
+        (AUTOMOTIVE, 'Automotive and Parts'),
+        (HEALTH, 'Health and Wellness')
+    )
+
+    categories = models.CharField(max_length=100, choices=CATEGORIES_CHOICES)
+
+    class Meta:
+        db_table = 'Category'
+        managed = True
+        verbose_name = 'Category'
+        verbose_name_plural = ''
+
+
 class Listing(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=1000)
-    starting_bid = models.FloatField(validators=[MinValueValidator(0.5)])
+    username = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    categories = models.ForeignKey(
+        Category, on_delete=models.CASCADE)
+    bid = models.FloatField(validators=[MinValueValidator(0.5)])
     web_page = models.URLField(max_length=200)
     img = models.ImageField(upload_to=None, height_field=None,
                             width_field=None, max_length=None)
     active = models.BooleanField()
-
-
-class Category(models.Model):
-    title = models.CharField(max_length=100)
-
-    class Meta:
-        db_table = ''
-        managed = True
-        verbose_name = 'ModelName'
-        verbose_name_plural = 'ModelNames'
--- 2-commerce/auctions/views.py
+++ 2-commerce/auctions/views.py
@@ -4,11 +4,16 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import Listing, User
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    listings = Listing.objects.all()
+    if listings == None:
+        return render(request, "auctions/index.html")
+    else:
+        context = {"listings": listings}
+        return render(request, "auctions/index.html", context)
 
 
 def login_view(request):
