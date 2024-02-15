93f2b0aef50199c0f92294a34fe7d8c914220edc
Nelson Mendez <105968684+nelsonmenza@users.noreply.github.com>
2024-02-15 12:28:17 -0700

    Added Class Like, and updated Class Post

--- 4-network/network/models.py
+++ 4-network/network/models.py
@@ -6,8 +6,16 @@ class User(AbstractUser):
     pass
 
 
+class Like(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    like = models.BooleanField(default=False)
+
+
 class Post(models.Model):
     username = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.TextField(max_length=240)
+    post_date = models.DateTimeField(auto_now_add=True)
+    like = models.ForeignKey(Like, on_delete=models.CASCADE)
 
 
 class Follower(models.Model):
