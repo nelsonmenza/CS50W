b539e772b21172fded7133b330aad166b9d6f897
Nelson Mendez <105968684+nelsonmenza@users.noreply.github.com>
2024-02-23 09:41:27 -0700

    Improved styles Index, layout login and register.

--- 4-network/network/models.py
+++ 4-network/network/models.py
@@ -2,15 +2,19 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+# The class `User` is a subclass of `AbstractUser` in Python.
 class User(AbstractUser):
     pass
 
 
+# The `Like` class represents a user's like on a particular item, with a boolean field indicating
+# whether the user has liked it or not.
 class Like(models.Model):
     username = models.ForeignKey(User, on_delete=models.CASCADE)
     like = models.BooleanField(default=False)
 
 
+# This class defines a Post model with fields for username, tweet content, post date, and likes.
 class Post(models.Model):
     username = models.ForeignKey(User, on_delete=models.CASCADE)
     tweet = models.TextField(max_length=240)
